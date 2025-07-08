3abe5642078754f152fe875b61ecedb2cade4c14
Yoram Halberstam <yoram.halberstam@gmail.com>
2025-07-08 21:45:30 +0300

    resolve platform web (3)

---
+++ lib/shamir_secret_plg_web.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+/// Web plugin stub (no native implementation needed)
+class ShamirSecretPlgWeb {
+  static void registerWith(Registrar registrar) {
+    // No-op: Pure Dart works on all platforms
+  }
+}
